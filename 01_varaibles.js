6f86bda4ddcadd2158a8c0139949eae53a563fff
mrriken <200625801@student.georgianc.on.ca>
2026-01-05 23:19:12 -0500

    making

---
+++ 01_varaibles.js
@@ -0,0 +1,8 @@
+// const var let
+
+const accountId = 2625
+var accountEmail = "rikenincanada@gmail.com"
+let accountPass = "1234"
+let accountState
+
+console.table([accountId,accountEmail,accountPass,accountState])
